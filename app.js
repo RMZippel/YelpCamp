dcc675731ea5dd39586f42ea5e8d9c35eca8a2a8
Rhys Zippel <rmzippel@gmail.com>
2018-10-31 21:21:59 +0000

    commented out database

--- app.js
+++ app.js
@@ -16,10 +16,9 @@ var express         = require("express"),
         campgroundRoutes = require("./routes/campgrounds"),
         authRoutes = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-
+mongoose.connect(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
-mongoose.connect("mongodb://rhys:1lovejess@ds147233.mlab.com:47233/yelpcamp");
+//mongoose.connect("mongodb://rhys:1lovejess@ds147233.mlab.com:47233/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
